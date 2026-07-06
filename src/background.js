0b853cc0452816707071e67834f09045eb70d689
vlakoff <544424+vlakoff@users.noreply.github.com>
2026-07-06 14:54:32 +0200

    Use semicolons
    Let's prevent against ASI pitfalls.

--- src/background.js
+++ src/background.js
@@ -1,53 +1,53 @@
-const layouts = ["standard", "wide", "vertical"]
-const options = ["folderPaneVisible", "messagePaneVisible"]
+const layouts = ["standard", "wide", "vertical"];
+const options = ["folderPaneVisible", "messagePaneVisible"];
 
 messenger.action.onClicked.addListener(async (t, e) => {
 	try {
-		let { skip } = (await messenger.storage.local.get({ skip: "none" }))
-		let filteredLayouts = layouts.filter(layout => layout !== skip)
-		let [currentTab] = await messenger.mailTabs.query({ active: true, currentWindow: true })
+		let { skip } = (await messenger.storage.local.get({ skip: "none" }));
+		let filteredLayouts = layouts.filter(layout => layout !== skip);
+		let [currentTab] = await messenger.mailTabs.query({ active: true, currentWindow: true });
 		if (currentTab) {
-			let index = filteredLayouts.findIndex(v => v === currentTab.layout)
-			index++
+			let index = filteredLayouts.findIndex(v => v === currentTab.layout);
+			index++;
 			if (index >= filteredLayouts.length) {
-				index = 0
+				index = 0;
 			}
 			await messenger.mailTabs.update({ layout: filteredLayouts[index] });
 		}
 	} catch (error) {
-		console.log(error)
+		console.log(error);
 	}
-})
+});
 
-const jobs = []
+const jobs = [];
 for (const layout of layouts) {
 	jobs.push(messenger.menus.create({
 		id: layout,
 		title: messenger.i18n.getMessage(layout),
 		contexts: ["action"],
-	}))
+	}));
 }
 jobs.push(messenger.menus.create({
 	id: "sep1",
 	type: "separator",
 	contexts: ["action"],
-}))
+}));
 for (const option of options) {
 	jobs.push(messenger.menus.create({
 		id: option,
 		title: messenger.i18n.getMessage(option),
 		contexts: ["action"],
-	}))
+	}));
 }
 Promise.all(jobs).then(() => {
 	messenger.menus.onClicked.addListener(async (info, tab) => {
-		let [currentTab] = await messenger.mailTabs.query({ active: true, currentWindow: true })
+		let [currentTab] = await messenger.mailTabs.query({ active: true, currentWindow: true });
 		if (currentTab) {
 			if (options.includes(info.menuItemId)) {
-				messenger.mailTabs.update({ [info.menuItemId]: !currentTab[info.menuItemId] })
+				messenger.mailTabs.update({ [info.menuItemId]: !currentTab[info.menuItemId] });
 			} else {
-				messenger.mailTabs.update({ layout: info.menuItemId })
+				messenger.mailTabs.update({ layout: info.menuItemId });
 			}
 		}
-	})
-})
+	});
+});
