82b7b66c95d369b202d5fc0172393383d6f6ff23
vlakoff <544424+vlakoff@users.noreply.github.com>
2026-07-06 14:53:17 +0200

    Spaces to tabs

--- src/options.js
+++ src/options.js
@@ -1,12 +1,12 @@
 try {
-    const { skip } = (await messenger.storage.local.get({ skip: 'none' }))
-    document.getElementById(skip).checked = true
-    let radios = document.querySelectorAll('input[type="radio"]');
-    radios.forEach(element => {
-        element.addEventListener("change", async e => {
-            messenger.storage.local.set({ skip: e.target.value })
-        })
-    });
+	const { skip } = (await messenger.storage.local.get({ skip: 'none' }))
+	document.getElementById(skip).checked = true
+	let radios = document.querySelectorAll('input[type="radio"]');
+	radios.forEach(element => {
+		element.addEventListener("change", async e => {
+			messenger.storage.local.set({ skip: e.target.value })
+		})
+	});
 } catch (error) {
-    console.log(error)
+	console.log(error)
 }
